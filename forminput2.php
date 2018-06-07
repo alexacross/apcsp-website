c7d51bee8f915f412727ca164fb59cb07f739950
Alexa Cross <alexa.acc3@gmail.com>
2018-06-07 22:28:29 +0000

    form

--- forminput2.php
+++ forminput2.php
@@ -15,7 +15,7 @@
 
        if ($_SERVER["REQUEST_METHOD"] == "POST") {
          $arg1 = test_input($_POST["arg1"]);
-         exec("/usr/lib/cgi-bin/student3/finalproject2 " . $arg1 . " " . $output, $retc);
+         exec("/usr/lib/cgi-bin/student3/compguess" . $arg1 . " " . $output, $retc);
        }
 
        function test_input($data) {
@@ -28,47 +28,6 @@
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
       Arg1: <input type="text" name="Secret Number"><br>
-      <input type="submit">
-    </form>
-
-    <?php
-       echo "<h2>Your Input:</h2>";
-       echo $arg1;
-       echo "<br>";
-       
-       echo "<h2>C Program Output:</h2>";
-       foreach ($output as $line) {
-         echo $line;
-         echo "<br>";
-       }
-       
-       echo "<h2>C Program Return Code:</h2>";
-       echo $retc;
-      
-     ?>
-
-    <h1>User Guess</h1>
-    <p>Guess the computer's secret number!</p>
-
-    <?php
-       // define variables and set to empty values
-       $arg1 = $output = $retc = "";
-
-       if ($_SERVER["REQUEST_METHOD"] == "POST") {
-         $arg1 = test_input($_POST["arg1"]);
-         exec("/usr/lib/cgi-bin/student3/finalproject " . $arg1 . " " . $output, $retc); 
-       }
-
-       function test_input($data) {
-         $data = trim($data);
-         $data = stripslashes($data);
-         $data = htmlspecialchars($data);
-         return $data;
-       }
-    ?>
-
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-      Arg1: <input type="text" name="Guess"><br>
       <input type="submit">
     </form>
 
