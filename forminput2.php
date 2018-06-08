bdce834e0f301abf89a9e257184e7d14859c7763
Alexa Cross <alexa.acc3@gmail.com>
2018-06-08 19:27:27 +0000

    forminput2.php

--- forminput2.php
+++ forminput2.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Binary Search: Secret Number</title>
+    <title>Form Input 2</title>
   </head>
+
 
   <body>
 
-    <h1>Computer Guess</h1>
-    <p>Enter a secret number and have the computer guess it!</p>
+    <h1>Form Input 2</h1>
+    <p>Demo of how to take form input and pass it to a C program - all in a single page</p>
 
     <?php
        // define variables and set to empty values
-       $arg1 = $output = $retc = "";
+       $arg1 = $arg2 = $output = $retc = "";
 
        if ($_SERVER["REQUEST_METHOD"] == "POST") {
          $arg1 = test_input($_POST["arg1"]);
-         exec("/usr/lib/cgi-bin/student3/compguess" . $arg1 . " " . $output, $retc);
+         $arg2 = test_input($_POST["arg2"]);
+         exec("/usr/lib/cgi-bin/student3/compguess " . $arg1 . " " . $arg2, $output, $retc); 
        }
 
        function test_input($data) {
@@ -27,23 +29,24 @@
     ?>
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-      Arg1: <input type="text" name="Secret Number"><br>
+      Arg1: <input type="text" name="arg1"><br>
+      Arg2: <input type="text" name="arg2"><br>
       <input type="submit">
     </form>
 
     <?php
        echo "<h2>Your Input:</h2>";
-       echo "<p>$arg1</p>";
+       echo "<p>$arg1<p>";
        echo "<br>";
        
-       echo "<h2>C Program Output:</h2>";
+       echo "<h2>C Program Output (an array):</h2>";
        foreach ($output as $line) {
-         echo "<p>$line</p>";
+         echo <p>$line</p>;
          echo "<br>";
        }
        
        echo "<h2>C Program Return Code:</h2>";
-       echo <p>$retc</p>;
+       echo "<p>$retc</p>;
       
      ?>
     
